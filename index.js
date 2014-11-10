cafcec30e45f3154fdf179c53450384dfe46e79d
Meesayen <gvg.fede@gmail.com>
2014-11-10 12:35:09 +0000

    Implemented latest es6-module-transpiler. Bumped esnext version. Clean up.

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 var through = require('through');
-// var convert = require('convert-source-map');
 var path = require("path");
 var esnext = require('esnext');
-var Compiler = require('es6-module-transpiler').Compiler;
+var transpiler = require('es6-module-transpiler');
+var recast     = require('es6-module-transpiler/node_modules/recast');
 
 function compile(file, content) {
   var
     moduleName = null,
     ext = path.extname(file),
-    compiler,
-    processed = null;
+    processed = null,
+    container = new transpiler.Container({
+      resolvers: [new transpiler.FileResolver([process.cwd()])],
+      formatter: new transpiler.formatters.commonjs()
+    });
 
   moduleName = file.slice(0, -ext.length).replace(/\\/g, '/');
 
-  compiler = new Compiler(String(content), moduleName, {});
+  var module = new transpiler.Module(file, file, container);
 
-  content = compiler['toCJS'].call(compiler);
+  module.name = moduleName;
+  module.src = content;
+  container.addModule(module);
+
+  content = recast.print(container.convert().pop()).code;
   file.contents = new Buffer(content);
 
   try {
     processed = esnext.compile(content, {bare: true}).code;
   } catch (e) {
-    // log.error('%s\n  at %s', e.message, file.originalPath);
+    console.error(e);
   }
-  // var comment = convert
-  //     .fromJSON(compiled.v3SourceMap)
-  //     .setProperty('sources', [ file ])
-  //     .toComment();
   return processed;
 }
 
@@ -38,21 +41,22 @@ module.exports = function (file, opts) {
   if (!isEsNext(file, opts && opts.fileExt || '.es6')) {
     return through();
   }
-  var data = '';
+  var
+    data = '',
+    write = function(buf) {
+      data += buf;
+    },
+
+    end = function() {
+      var src;
+      try {
+        src = compile(file, data);
+      } catch (error) {
+        this.emit('error', error);
+      }
+      this.queue(src);
+      this.queue(null);
+    };
+
   return through(write, end);
-
-  function write(buf) {
-    data += buf;
-  }
-
-  function end() {
-    var src;
-    try {
-      src = compile(file, data);
-    } catch (error) {
-      this.emit('error', error);
-    }
-    this.queue(src);
-    this.queue(null);
-  }
 };
